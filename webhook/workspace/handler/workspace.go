04c544f1981dd3f0f6f4c1ab2dfa21dfff94a08a
Anatolii Bazko <abazko@redhat.com>
2026-08-17 17:12:51 +0200

    chore: Allows to stop broken workspaces
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- webhook/workspace/handler/workspace.go
+++ webhook/workspace/handler/workspace.go
@@ -170,12 +170,21 @@ func (h *WebhookHandler) MutateWorkspaceV1alpha2OnUpdate(ctx context.Context, re
 		return admission.Denied(msg)
 	}
 
-	changed, code, err := h.ValidateWorkspaceV1alpha2Permissions(ctx, newWksp, oldWksp, req)
-	if err != nil {
-		if code != nil {
-			return admission.Errored(*code, err)
+	// Indicates, if DevWorkspace object has been updated during validation
+	changed := false
+
+	isTransitionFromStartToStop := !newWksp.Spec.Started && oldWksp.Spec.Started
+	// Skip permission validation when stopping a workspace: users must always
+	// be able to stop a workspace even if its current spec would fail validation (e.g. after RBAC changes).
+	if !isTransitionFromStartToStop {
+		var code *int32
+		changed, code, err = h.ValidateWorkspaceV1alpha2Permissions(ctx, newWksp, oldWksp, req)
+		if err != nil {
+			if code != nil {
+				return admission.Errored(*code, err)
+			}
+			return admission.Denied(err.Error())
 		}
-		return admission.Denied(err.Error())
 	}
 
 	oldCreator, found := oldWksp.Labels[constants.DevWorkspaceCreatorLabel]
